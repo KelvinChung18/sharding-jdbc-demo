587367abd74385cb1ace2a1925bfe43e6c5ea4ee
zhonghaofeng <>
2024-09-06 15:53:13 +0800

    add 测试main分支提交是否有权限1552

--- sharding-jdbc-4x/src/main/java/com/kelvin/shardingjdbc4x/ShardingJdbc4xApplication.java
+++ sharding-jdbc-4x/src/main/java/com/kelvin/shardingjdbc4x/ShardingJdbc4xApplication.java
@@ -2,7 +2,7 @@ package com.kelvin.shardingjdbc4x;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// 123
+// 1552
 @SpringBootApplication
 public class ShardingJdbc4xApplication {
 
