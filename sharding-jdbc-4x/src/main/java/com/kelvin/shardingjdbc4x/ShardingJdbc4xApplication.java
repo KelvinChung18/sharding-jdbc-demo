238d2fb0803eac6e1ef9a5a448bb28434ecbc919
zhonghaofeng <>
2024-09-06 15:48:37 +0800

    add 测试main分支提交是否有权限

--- sharding-jdbc-4x/src/main/java/com/kelvin/shardingjdbc4x/ShardingJdbc4xApplication.java
+++ sharding-jdbc-4x/src/main/java/com/kelvin/shardingjdbc4x/ShardingJdbc4xApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 123
 @SpringBootApplication
 public class ShardingJdbc4xApplication {
-    // 测试强行提交main分支 123
+
     public static void main(String[] args) {
         SpringApplication.run(ShardingJdbc4xApplication.class, args);
     }
