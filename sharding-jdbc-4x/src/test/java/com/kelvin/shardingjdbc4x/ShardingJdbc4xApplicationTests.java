270292f9e2302bd0e2b36467d6e9af8d77d6bcad
zhonghaofeng <>
2024-09-06 14:27:42 +0800

    add 项目初始化

---
+++ sharding-jdbc-4x/src/test/java/com/kelvin/shardingjdbc4x/ShardingJdbc4xApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.shardingjdbc4x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingJdbc4xApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
